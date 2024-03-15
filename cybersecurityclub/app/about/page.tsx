d1f3c560db5881996920e431aff240f27959d660
wcward3302 <wcward3302@eagle.fgcu.edu>
2024-03-15 12:59:34 -0400

    Add 'about' and 'contact' page, update global CSS

--- cybersecurityclub/app/about/page.tsx
+++ cybersecurityclub/app/about/page.tsx
@@ -1,15 +1,47 @@
 import Head from 'next/head';
-
+//import Navbar from './component/Navigation/Navbar';
+import Navbar from '../component/Navigation/Navbar';
 
 
 export default function About() {
   return (
-    <div>
+    <html>
       <Head>
         <title>About Us</title>
       </Head>
-      <h1 className='aboutUsTitle'>About Us</h1>
-      <p className = 'introTitle'>Flordia Gulf Coast University, Home of the Eagles</p>
-    </div>
-  );
+    
+      <body className='bodyBackground'>
+      <div className="min-h-screen flex flex-col"> {/* Set container as a flex container */}
+      <Navbar />
+
+      <div className='topHeadBox'>
+        <h1 className='pageDiscFont'>About Us</h1>
+        <p className = 'fgcuNameFont'>Flordia Gulf Coast University, Home of the Eagles</p>
+        </div>
+      
+        <div className = "pageItemDiscBox">
+          <p id = 'clubLeaders'>Cybersecurity Club Leadership Members</p>
+        </div>
+
+        <div className = "pageItemBox">
+          <p id = 'president'>Club President: Niel Patel <br></br>Contact: <a href = "nkpatel5457@eagle.fgcu.edu">nkpatel5457@eagle.fgcu.edu</a></p>
+        </div>
+        <div className = "pageItemBox">
+          <p id = 'vicePres'>Club Vice President: Caleb Newman <br></br>Contact: <a href = "cjnewman6275@eagle.fgcu.edu">cjnewman6275@eagle.fgcu.edu</a></p>
+        </div>
+        <div className = "pageItemBox">
+          <p id = 'secretary'>Club Secretary: William Ward <br></br>Contact: <a href = "wcward3302@eagle.fgcu.edu">wcward3302@eagle.fgcu.edu</a></p>
+        </div>
+        <div className = "pageItemBox">
+          <p id = 'treasurer'>Club Treasurer: Johnny Mai <br></br>Contact: <a href = "jfmai2313@eagle.fgcu.edu">jfmai2313@eagle.fgcu.edu</a></p>
+        </div>
+      </div>
+
+        
+
+      </body>
+
+    </html>
+      
+    );
 }
--- cybersecurityclub/app/contact/page.tsx
+++ cybersecurityclub/app/contact/page.tsx
@@ -1,7 +1,34 @@
+import Head from 'next/head';
+//import Navbar from './component/Navigation/Navbar';
+import Navbar from '../component/Navigation/Navbar';
+
 export default function Contact (){
-    return(
-        <main>
-            <h2>Contact Us</h2>
-        </main>
-    )
+return(
+<main>
+    <html>
+        <Head>
+            <title>Contact Us</title>
+        </Head>
+
+        <body className='bodyBackground'>
+            <div className="min-h-screen flex flex-col"> {/* Set container as a flex container */}
+            <Navbar />
+
+                <div className='topHeadBox'>
+                    <h1 className='pageDiscFont'>Contact Us!</h1>
+                    <p className = 'fgcuNameFont'>Flordia Gulf Coast University, Home of the Eagles</p>
+                </div>
+                <div className = "pageItemBox">
+                    <p id = 'contactMessage'> - If you are an FGCU Student interested in joining the FGCU Cybersecurity Club<br></br>
+                        - If you wish to get in touch to plan / discuss the seminars or clinics hosted by the FGCU Cybersecurity club<br></br>
+                        Please don't hesitate to reach out! Contact our secretary and we will invite you to the team or return your message as soon as possible. <br></br>
+                        <br></br>Thank you - Cybersecurity Club Leadership <br></br>
+                        <br></br>Contact info: <a href = "wcward3302@eagle.fgcu.edu">wcward3302@eagle.fgcu.edu</a><br></br>
+                    </p>
+                </div>
+            </div>
+        </body>
+    </html>
+</main>
+)
 }
